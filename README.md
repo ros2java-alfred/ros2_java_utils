766d2847e4249d992113a2fb80e9191ff740cfc2
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2016-10-02 03:22:03 +0200

    Added new topic command

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # rcljava_utils
+
+rostopic_java
+```
+rostopic_java is a command-line tool for printing information about ROS2 Topics.
+Commands:
+    rostopic echo   print messages to screen
+    rostopic find   find topics by type
+    rostopic hz     display publishing rate of topic
+    rostopic list   list active topics
+    rostopic pub    publish data to topic
+    rostopic type   print topic type
+Type rostopic_java <command> -h for more detailed usage, e.g. 'rostopic echo -h'
+```
+
+### List active topics
+`rostopic_java list`
+
+### Print topic type
+`rostopic_java type /topic`
+
+### Find topics by type
+`rostopic_java find msgs/Message`
+
+### Print messages to screen
+`rostopic_java echo /topic msgs/Message`
+
+### Publish data to topic
+`rostopic_java pub /topic msgs/Message '{message}'`
+
--- rcljava_utils/src/main/java/org/ros2/rcljava/tool/RosTopics.java
+++ rcljava_utils/src/main/java/org/ros2/rcljava/tool/RosTopics.java
@@ -2,18 +2,22 @@ package org.ros2.rcljava.tool;
 
 import java.util.HashMap;
 import java.util.Map.Entry;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
+import org.ros2.rcljava.QoSProfile;
 import org.ros2.rcljava.RCLJava;
+import org.ros2.rcljava.internal.message.Message;
 import org.ros2.rcljava.node.Node;
+import org.ros2.rcljava.node.topic.Consumer;
+import org.ros2.rcljava.node.topic.Publisher;
+import org.ros2.rcljava.node.topic.Subscription;
+
+import com.google.gson.Gson;
 
 
 public class RosTopics {
 
-    private static Logger logger = Logger.getLogger(RCLJava.LOG_NAME);
+    private static String NAME = RosTopics.class.getSimpleName();
+//    private static Logger logger = Logger.getLogger(RCLJava.LOG_NAME);
 
     private final static String CMD_ECHO    = "echo";
     private final static String CMD_HZ      = "hz";
@@ -28,12 +32,12 @@ public class RosTopics {
     private static void fullUsage() {
         System.out.println("rostopic is a command-line tool for printing information about ROS Topics.\n" +
         "Commands:\n" +
-        "\trostopic bw\tdisplay bandwidth used by topic\n" +
-        "\trostopic delay\tdisplay delay of topic from timestamp in header\n" +
+//        "\trostopic bw\tdisplay bandwidth used by topic\n" +
+//        "\trostopic delay\tdisplay delay of topic from timestamp in header\n" +
         "\trostopic echo\tprint messages to screen\n" +
         "\trostopic find\tfind topics by type\n" +
-        "\trostopic hz\tdisplay publishing rate of topic    \n" +
-        "\trostopic info\tprint information about active topic\n" +
+//        "\trostopic hz\tdisplay publishing rate of topic    \n" +
+//        "\trostopic info\tprint information about active topic\n" +
         "\trostopic list\tlist active topics\n" +
         "\trostopic pub\tpublish data to topic\n" +
         "\trostopic type\tprint topic type\n" +
@@ -41,24 +45,45 @@ public class RosTopics {
         System.exit(1);
     }
 
-    private static void rostopicCmdHz() {
+    private static void rostopicCmdHz(String[] args) {
         //TODO not implemented.
         System.out.println("Not implemented...");
     }
 
-    private static void rostopicCmdType() {
-        //TODO not implemented.
-        System.out.println("Not implemented...");
+    private static void rostopicCmdType(String[] args) {
+        Node node = RCLJava.createNode(NAME);
+
+        if (args.length < 2) {
+            System.out.println("Need to add topic name.");
+        } else
+        if (args.length > 2) {
+            System.out.println("You may only specify one input topic.");
+        } else {
+
+            String topicPath = args[1];
+            HashMap<String, String > topicsTypes = node.getTopicNamesAndTypes();
+
+            if (topicsTypes.size() > 0) {
+                if (topicsTypes.containsKey(topicPath)) {
+                    System.out.println(String.format("%s", topicsTypes.get(topicPath)));
+                } else {
+                    System.out.println("No Topic " + topicPath + " available !");
+                }
+            }else {
+                System.out.println("No Topics available !");
+            }
+        }
+
+        node.dispose();
     }
 
-    private static void rostopicCmdList() {
-        // Let's create a Node
-        Node node = RCLJava.createNode("_debug");
+    private static void rostopicCmdList(String[] args) {
+        Node node = RCLJava.createNode(NAME);
         HashMap<String, String > topicsTypes = node.getTopicNamesAndTypes();
 
         if (topicsTypes.size() > 0) {
             for (Entry<String, String> entity : topicsTypes.entrySet()) {
-                    System.out.println(String.format("%s [%s]", entity.getKey(), entity.getValue()));
+                System.out.println(String.format("%s", entity.getKey()));
             }
         }else {
             System.out.println("Empty topics !");
@@ -67,37 +92,133 @@ public class RosTopics {
         node.dispose();
     }
 
-    private static void rostopicCmdInfo() {
+    private static void rostopicCmdInfo(String[] args) {
         //TODO not implemented.
         System.out.println("Not implemented...");
     }
 
-    private static void rostopicCmdPub() {
+    private static void rostopicCmdPub(String[] args) {
+        Node node = RCLJava.createNode(NAME);
+
+        if (args.length == 1) {
+            System.out.println("/topic must be specified");
+        } else
+        if (args.length == 2) {
+            System.out.println("topic type must be specified");
+        } else
+        if (args.length == 3) {
+            System.out.println("message must be specified");
+        } else {
+
+            final Gson gson = new Gson();
+            String topic = args[1];
+            String messageTypeName = args[2];
+            String messageJson = args[3];
+
+            Class<Message> messageType = RosTopics.loadMessage(messageTypeName);
+            Message message = gson.fromJson(messageJson, messageType);
+
+            Publisher<Message> pub =
+                    node.<Message>createPublisher(
+                        messageType,
+                        topic,
+                        QoSProfile.PROFILE_DEFAULT);
+
+            while(RCLJava.ok()) {
+                System.out.println("Publishing: " + gson.toJson(message));
+                pub.publish(message);
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            pub.dispose();
+        }
+
+        node.dispose();
+    }
+
+    private static void rostopicCmdBw(String[] args) {
         //TODO not implemented.
         System.out.println("Not implemented...");
     }
 
-    private static void rostopicCmdBw() {
+    private static void rostopicCmdFind(String[] args) {
+        Node node = RCLJava.createNode(NAME);
+
+        if (args.length == 1) {
+            System.out.println("topic type must be specified");
+        } else {
+            String nametype = args[1];
+            HashMap<String, String > topicsTypes = node.getTopicNamesAndTypes();
+
+            if (topicsTypes.containsValue(nametype)) {
+                for (Entry<String, String> entity : topicsTypes.entrySet()) {
+                    if (entity.getValue().equals(nametype)) {
+                        System.out.println(String.format("%s", entity.getKey()));
+                    }
+                }
+            }
+        }
+
+        node.dispose();
+    }
+
+    private static void rostopicCmdDelay(String[] args) {
         //TODO not implemented.
         System.out.println("Not implemented...");
     }
 
-    private static void rostopicCmdFind() {
-        //TODO not implemented.
-        System.out.println("Not implemented...");
+    private static void rostopicCmdEcho(String[] args) {
+        Node node = RCLJava.createNode(NAME);
+
+        if (args.length == 1) {
+            System.out.println("/topic must be specified");
+        }
+        else if (args.length == 2) {
+            System.out.println("topic type must be specified");
+        } else {
+            String topic = args[1];
+            String messageTypeName = args[2];
+
+            final Class<Message> messageType = RosTopics.loadMessage(messageTypeName);
+            final Gson gson = new Gson();
+
+            Subscription<Message> sub = node.<Message>createSubscription(
+                    messageType,
+                    topic,
+                    new Consumer<Message>() {
+                        @Override
+                        public void accept(Message msg) {
+                            System.out.println(gson.toJson(msg));
+                        }
+                    },
+                    QoSProfile.PROFILE_DEFAULT);
+
+            RCLJava.spin(node);
+
+            sub.dispose();
+            }
+
+        node.dispose();
     }
 
-    private static void rostopicCmdDelay() {
-        //TODO not implemented.
-        System.out.println("Not implemented...");
+    @SuppressWarnings("unchecked")
+    private static Class<Message> loadMessage(String messageTypeName) {
+        Class<Message> messageType = null;
+
+        try {
+            messageType = (Class<Message>) Class.forName(messageTypeName.replaceFirst("/", ".msg."));
+        } catch (ClassNotFoundException e1) {
+            System.out.println("Message not found !");
+            e1.printStackTrace();
+        }
+
+        return messageType;
     }
-
-    private static void rostopicCmdEcho() {
-        //TODO not implemented.
-        System.out.println("Not implemented...");
-    }
-
-
 
     public static void main(String[] args) throws InterruptedException {
 //        logger.setLevel(Level.ALL);
@@ -116,38 +237,39 @@ public class RosTopics {
         try {
             switch (args[0]) {
             case CMD_ECHO:
-                rostopicCmdEcho();
+                RosTopics.rostopicCmdEcho(args);
                 break;
             case CMD_HZ:
-                rostopicCmdHz();
+                RosTopics.rostopicCmdHz(args);
                 break;
             case CMD_TYPE:
-                rostopicCmdType();
+                RosTopics.rostopicCmdType(args);
                 break;
             case CMD_LIST:
-                rostopicCmdList();
+                RosTopics.rostopicCmdList(args);
                 break;
             case CMD_INFO:
-                rostopicCmdInfo();
+                RosTopics.rostopicCmdInfo(args);
                 break;
             case CMD_PUB:
-                rostopicCmdPub();
+                RosTopics.rostopicCmdPub(args);
                 break;
             case CMD_BW:
-                rostopicCmdBw();
+                RosTopics.rostopicCmdBw(args);
                 break;
             case CMD_FIND:
-                rostopicCmdFind();
+                RosTopics.rostopicCmdFind(args);
                 break;
             case CMD_DELAY:
-                rostopicCmdDelay();
+                RosTopics.rostopicCmdDelay(args);
                 break;
             default:
-                fullUsage();
+                RosTopics.fullUsage();
                 break;
             }
         } catch (Exception e) {
             System.out.println("ERROR : " + e.getMessage());
+            e.printStackTrace();
         }
 
         RCLJava.shutdown();
