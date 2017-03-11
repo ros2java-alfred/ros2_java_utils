1b340acf3f4e057bae9b39b5caf668234779d47e
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2017-03-11 21:24:53 +0100

    Fixed coverall

--- rcljava_utils/src/test/java/com/ros2/rcljava/tool/TestRos2Topics.java
+++ rcljava_utils/src/test/java/com/ros2/rcljava/tool/TestRos2Topics.java
@@ -2,15 +2,12 @@ package com.ros2.rcljava.tool;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.ros2.rcljava.RCLJava;
 import org.ros2.rcljava.tool.Ros2Topics;
 
 public class TestRos2Topics {
@@ -39,8 +36,38 @@ public class TestRos2Topics {
     }
 
     @Test
+    public final void testEcho() throws InterruptedException {
+        Ros2Topics.main(new String[]{"echo"});
+        assertEquals(true, true);
+    }
+
+    @Test
+    public final void testFind() throws InterruptedException {
+        Ros2Topics.main(new String[]{"find"});
+        assertEquals(true, true);
+    }
+
+    @Test
+    public final void testHz() throws InterruptedException {
+        Ros2Topics.main(new String[]{"hz"});
+        assertEquals(true, true);
+    }
+
+    @Test
     public final void testList() throws InterruptedException {
         Ros2Topics.main(new String[]{"list"});
+        assertEquals(true, true);
+    }
+
+    @Test
+    public final void testPub() throws InterruptedException {
+        Ros2Topics.main(new String[]{"pub"});
+        assertEquals(true, true);
+    }
+
+    @Test
+    public final void testType() throws InterruptedException {
+        Ros2Topics.main(new String[]{"type"});
         assertEquals(true, true);
     }
 
